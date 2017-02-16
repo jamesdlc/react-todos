0a8199c592466e8269a1c2e9320ea794d512f612
jamesdlc <jamesdelacruz@gmail.com>
2017-02-16 13:28:55 -0800

    added testing suite tested if components exist and if todoList components render iterations

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Take out the trash'
+        },
+        {
+          id: 2,
+          text: 'Wash the car'
+        },
+        {
+          id: 3,
+          text: 'Buy milk'
+        },
+        {
+          id: 4,
+          text: 'Call Sarah'
+        }
+      ]
+    };
+  },
   render: function () {
+    var {todos} = this.state;
+
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos} />
       </div>
     )
   }
